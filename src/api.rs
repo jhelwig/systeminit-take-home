36f61ea0b6f37b70a8cefd979a1e864c137921c2
Jacob Helwig <jacob@technosorcery.net>
2021-12-04 12:22:17 -0800

    Add basic API tests

--- src/api.rs
+++ src/api.rs
@@ -60,3 +60,127 @@ async fn new_message(
 
     list_messages(Extension(app_state)).await
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{
+            self,
+            Request,
+        },
+    };
+    use tower::ServiceExt;
+
+    #[tokio::test]
+    async fn get_no_messages() {
+        let app_state = Arc::new(AppState { messages: RwLock::new(vec![]) });
+        let router = get_router(app_state);
+
+        let response = router
+            .oneshot(Request::get("/messages").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let expected_body: Vec<String> = vec![];
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!(expected_body));
+    }
+
+    #[tokio::test]
+    async fn get_some_messages() {
+        let app_state = Arc::new(
+        AppState {
+                messages: RwLock::new(vec![
+                    "Message 1".into(),
+                    "Message 2".into()
+                ])
+            }
+        );
+        let router = get_router(app_state);
+
+        let response = router
+            .oneshot(Request::get("/messages").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let expected_body: Vec<String> = vec![
+            "Message 1".into(),
+            "Message 2".into(),
+        ];
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!(expected_body));
+    }
+
+    #[tokio::test]
+    async fn add_new_message() {
+        let app_state = Arc::new(AppState {messages: RwLock::new(vec![])});
+        let router = get_router(app_state.clone());
+
+        let body_content = "The new message";
+        let response = router
+            .oneshot(
+                Request::post("/messages")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .header(http::header::CONTENT_LENGTH, body_content.bytes().count())
+                    .body(Body::from(body_content))
+                    .unwrap()
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let expected_messages: Vec<String> = vec![
+            "The new message".into(),
+        ];
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!(expected_messages));
+
+        assert_eq!(
+            app_state.messages.read().unwrap().clone(),
+            expected_messages,
+        );
+    }
+
+    #[tokio::test]
+    async fn add_new_message_with_existing() {
+        let app_state = Arc::new(AppState {messages: RwLock::new(vec!["Existing message".into()])});
+        let router = get_router(app_state.clone());
+
+        let body_content = "The new message";
+        let response = router
+            .oneshot(
+                Request::post("/messages")
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .header(http::header::CONTENT_LENGTH, body_content.bytes().count())
+                    .body(Body::from(body_content))
+                    .unwrap()
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let expected_messages: Vec<String> = vec![
+            "Existing message".into(),
+            "The new message".into(),
+        ];
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!(expected_messages));
+
+        assert_eq!(
+            app_state.messages.read().unwrap().clone(),
+            expected_messages,
+        );
+    }
+}
